954d618ee93abfe198c1426e263f9f4cbaede675
Hayden Chen <hayden.chen.dev@gmail.com>
2024-07-05 13:13:10 +0800

    feat: :sparkles: add react-query

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   ...require("@haydenull/fabric/prettier"),
   // docs: https://github.com/tailwindlabs/prettier-plugin-tailwindcss#sorting-classes-in-function-calls
-  tailwindConfig: '.your-path/tailwind.config.js',
-  tailwindFunctions: [], // ['cn', 'clsx']
+  tailwindConfig: './tailwind.config.ts',
+  tailwindFunctions: ['cn', 'clsx'],
 }
